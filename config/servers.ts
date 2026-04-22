8ede76ced0c4d0ce3ee3f4a631300a821ad985ab
Mayank Jaat <153971576+Mayankjat005@users.noreply.github.com>
2026-04-22 23:53:03 +0530

    Update servers.ts

--- config/servers.ts
+++ config/servers.ts
@@ -1,7 +1,7 @@
 export const SERVERS = [
-  "https://cdn.tmovies.workers.dev",
-  "https://multi.starfilx.in.net", // User can add more here
-  "https://solo.starfilx.in.net",
+  "https://cdn.tmoviecdn1.workers.dev",
+  "https://cdn.tmoviecdn2.workers.dev", // User can add more here
+  "https://cdn.tmoviecdn3.workers.dev",
 ];
 
 export const FALLBACK_MESSAGE = "This Movie/TV Show Not Available. Change the server to Continue Streaming.";
